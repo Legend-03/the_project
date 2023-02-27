a58da683b433cce889ed6c6ba428c93123c59ea3
Legend-03 <nazareth.magnus@gmail.com>
2023-02-27 11:49:57 +0530

    second commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:pages/util/habit_objects.dart';
+
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,44 +13,92 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //List of Habit Functionality
+  // Datastructure
+  List habitList = [
+    // [habitName, habitStarted, timeSpent, timeGoal]
+    ['Sleep', false, 0, 20],
+    ['Walk', false, 0, 20],
+    ['Read', false, 0, 20],
+    ['Drink Water', false, 0, 20],
+  ];
+
+  void habitStarted(int index) {
+
+    //start timr
+    var startTime = DateTime.now();
+
+    
+
+    setState(() {
+      // creating contrast between pause & play with onTap
+      habitList[index][1] =!habitList[index][1];
+
+      // Time going
+      Timer.periodic(Duration(seconds: 1), (timer){
+        setState(() {
+          // stop timer
+        if(!habitList[index][1]){
+          timer.cancel();   //Timer will run until this condition is satisfied
+        }
+
+        //Elapsed time
+        var currentTime = DateTime.now();
+        habitList[index][2] = currentTime.second - startTime.second;
+        });
+
+
+
+      });
+
+    });
+  }
+  void settingsOpened(int index){
+    showDialog(context: context, builder: (context){
+      // alert as per the habitName: habitList-->0
+      return AlertDialog(title: Text('Settings for '+ habitList[index][0]));
+      });  
+    }
+    
+      
+  
+
   @override
   Widget build(BuildContext context) {
+    
+    // var habitList;
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        backgroundColor: Colors.teal[900],
-        title: Text('Your Habits'),
-        
-      ),
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          backgroundColor: Colors.teal[900],
+          title: const Text('Your Habits'),
+        ),
 
 // ----------------------------------------------------------
-      body: Column(
-        children: [
+        body: ListView.builder(
+          // Items to build
+          itemCount: habitList.length,
+
+          itemBuilder: ((context, index){
+          return HabitObject(   //Object- oriented programming
+              // Datatype: List
+              habitName: habitList[index][0],
+              onTap: () {
+                habitStarted(index);
+              },
+              settingsTap: () {
+                settingsOpened(index);
+              },
+              
+              habitStarted: habitList[index][1],
+              timeSpent: habitList[index][2],
+              timeGoal: habitList[index][3],
+              
+            );
           
-          
-          //Object- oriented programming
-          HabitObject(
-            habitName: 'Sleep',
-          ),
-
-          HabitObject(
-            habitName: 'Read',
-          ),
-          HabitObject(
-            habitName: 'Walk',
-          ),
-
-          HabitObject(
-            habitName: 'Hydrate',
-          ),
-          
-          HabitObject(
-            habitName: 'Hydrate',
-          ),
-        ],
-      )
-
-
-    );
+        })));
   }
-}
+  
+}  
+  
+
--- lib/util/habit_objects.dart
+++ lib/util/habit_objects.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
+
+
 class HabitObject extends StatelessWidget {
-
   final String habitName;
-  // final VoidCallback  onTap;
+  final VoidCallback  onTap;
+  final VoidCallback  settingsTap;
+  final int timeSpent;
+  final int timeGoal;
+  final bool habitStarted;  //play/pause button
 
-  const HabitObject({Key? key, required this.habitName}) : super(key: key);
+
+// Constructors for variables
+  const HabitObject({
+    Key? key, 
+    required this.habitName, 
+    required this.onTap, 
+    required this.settingsTap, 
+    required this.timeSpent,
+    required this.timeGoal,
+    required this.habitStarted,
+    
+    }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,45 +41,49 @@ class HabitObject extends StatelessWidget {
               children: [
                 //Row: To group progress circle & Habit name
                 Row(children: [
-
                   //progresssssss indicatorrrrrrrrrr
-                  Container(
-                    height:60, width:60,
-                    // child: SingleChildScrollView(
-                      
-                    
-                    child: Stack(children: [              //stack
-                      //circle
-                      CircularPercentIndicator(radius: 30),
-                      // error zoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-                      Center(child: SingleChildScrollView(child: Icon(Icons.play_arrow_outlined)))
-                    ])
-                      
-                    
+                  GestureDetector(
+                    onTap: onTap,
+                    child: SizedBox(
+                        height: 60,
+                        width: 60,
+                        // child: SingleChildScrollView(
+                  
+                        child: Stack(children: [
+                          //stack
+                          //circle
+                          CircularPercentIndicator(radius: 30),
+                          // error zoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+                          Center(
+                              child: SingleChildScrollView(
+                                  child: Icon(
+                                    habitStarted ? Icons.pause : Icons.play_arrow_outlined)))
+                        ])),
                   ),
 
-
                   // ---------------------------------------------------------
-                  const SizedBox(width:20),
+                  const SizedBox(width: 20),
                   Column(
                       // align in the beginning
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         //habit name
                         Text(habitName,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18)),
+                            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
 
-                        const SizedBox(height: 5), //Creating a space between habitName & Progress
+//Creating a space between habitName & Progress
+                        const SizedBox(height: 5), 
 
                         //progress
-                        const Text('2:00 / 10:00 = 20%',
-                            style: TextStyle(
-                              color: Colors.grey,
-                            )),
+                        Text(timeSpent.toString()+'/'+timeGoal.toString(),
+                            style: TextStyle(color: Colors.grey,)),
+                            
                       ]),
                 ]),
-                Icon(Icons.settings),
+                GestureDetector(
+//passing settingsTapped function
+                  onTap: settingsTap, 
+                  child: Icon(Icons.settings)),
               ])),
     );
   }
